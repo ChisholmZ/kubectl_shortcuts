91159057dba639e8c6fc6a511b4bc5b56b8534ad
Chisholm <ChisholmZ@schneider.com>
2019-05-13 16:27:54 -0500

    add telhub

--- lib/jobs.py
+++ lib/jobs.py
@@ -6,5 +6,5 @@ def create_job(args, jenkins_url):
 
     data = re.sub(r'(ps-)?heartbeat-(ps-)?adapter', args.job, xml.content.decode('utf-8')).encode('utf-8')
 
-    response = requests.post("http://%s/job/TelematicsHub-%s/createItem?name=%s" % (jenkins_url, args.context, args.job), data=data, headers={"Content-Type":"application/xml"})
+    response = requests.post("http://%s/job/TelematicsHub-%s/createItem?name=telhub-%s" % (jenkins_url, args.context, args.job), data=data, headers={"Content-Type":"application/xml"})
     print(response)
