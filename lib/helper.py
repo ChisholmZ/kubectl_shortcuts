301c02b550e862ebf9418de20d14e87c16c398e0
Chisholm <ChisholmZ@schneider.com>
2019-05-23 14:35:27 -0500

    update jenkins job code

--- lib/helper.py
+++ lib/helper.py
@@ -5,13 +5,31 @@ def choose_jobs(jobs):
     for job in jobs:
         print(job)
 
-# get services from json to fix names of repos
+# get json
+def get_json():
+    with open(os.path.join(sys.path[0],'files','telhub.json')) as file:
+        data = json.loads(file.read())
+    return data
+
+# get services from json to fix names
 def get_services():
-    with open(os.path.join(sys.path[0],'lib','telhub.json')) as file:
-        services = json.loads(file.read())
-    return services
+    return get_json()['services']
+
+# get repo from json
+def get_repos():
+    return get_json()['repos']
+
+def get_repo(job):
+    job = re.sub(r'(telematics-hub|telhub)[-_]', '', job)
+    repos = get_repos()
+    return repos[job] if job in repos.keys() else get_service_from_job(job)
 
 def get_service(service):
     service = re.sub(r'(telematics-hub|telhub)[-_]', '', service)
     services = get_services()
     return services[service] if service in services.keys() else service
+
+def get_service_from_job(job):
+    job = re.sub(r'(telematics-hub|telhub)[-_]', '', job)
+    service = [k for k,v in get_services().items() if v == job]
+    return service[0] if len(service) else job
--- lib/jobs.py
+++ lib/jobs.py
@@ -2,33 +2,52 @@ import requests, re, jenkins
 from lib.helper import *
 
 class Jobs():
-    def __init__(self, jenkins, context):
-        url = 'http://%s/job/TelematicsHub-%s' % (jenkins['domain'], context)
-        self.server = jenkins.Jenkins(url, username=jenkins['user'], password=jenkins['token'])
+    def __init__(self, config, context):
+        url = 'http://%s/job/TelematicsHub-%s' % (config['domain'], context)
+        self.server = jenkins.Jenkins(url, username=config['user'], password=config['token'])
+        path = os.path.join(sys.path[0],'files','job_%s.xml' % context.lower())
+        if os.path.isfile(path):
+            self.xml = open(path, 'r').read()
+        self.context = context
 
-    #create jenkins job base on existing job
+    # create jenkins job base on existing job
     def create_job(self, job):
         xml = self.update_xml(job)
         self.server.create_job(job, xml)
 
+    # build jenkins job
     def build(self, job, params = {}):
         self.server.build_job(job, params)
 
+    # get jenkins jobs
     def get_jobs(self):
         return self.server.get_jobs(folder_depth=0)
 
+    # update the jenkins xml
     def update_xml(self, job):
-        service = get_service(job)
         job = re.sub(r'telhub[-_]', '', job)
-        heartbeat = self.server.get_job_config('telhub-ps-heartbeat-adapter')
-        return heartbeat.replace('ps-heartbeat-adapter', job).replace('heartbeat-ps-adapter', service)
+        service = get_service(job)
+        repo = get_repo(job)
+        return self.xml.replace('#JOB#', job).replace('#SERVICE#', service).replace('#REPO#', repo)
 
+    # update job
+    def update_job(self, job):
+        xml = self.update_xml(job)
+        self.server.reconfig_job(job, xml)
+
+    # update all the jobs
     def update_jobs(self):
         for job in self.get_jobs():
             if re.match(r'^hudson\.model', job['_class']) and job['name'] != 'telhub_devops':
-                print(job['name'])
-                xml = self.update_xml(job['name'])
-                self.server.reconfig_job(job['name'], xml)
+                update_job(job['name'])
+
+    def save_config(self):
+        xml = self.server.get_job_config('telhub-heartbeat-ps-adapter')
+        xml = xml.replace('ps-heartbeat-adapter<','#JOB#<')
+        xml = xml.replace('=heartbeat-ps-adapter', '=#SERVICE#')
+        xml = xml.replace('ps-heartbeat-adapter.git','#REPO#.git')
+        path = os.path.join(sys.path[0],'files','job_%s.xml' % self.context.lower())
+        open(path, 'w').write(xml)
 
     # def select_job(self):
     #     choices = []
