53f6c003539bf035b77de079ac04639c9caea26b
Zach Chisholm <ChisholmZ@Schneider.com>
2019-05-29 11:18:35 -0500

    check for release branch

--- lib/yaml.py
+++ lib/yaml.py
@@ -15,27 +15,49 @@ def get_jobs(github_token):
             services.update({service: service})
     return services
 
+# loop through branchs to see if it exists
+def get_branch(repo, release):
+    for branch in repo.get_branches():
+        if release == branch.name:
+            return True
+    return False
+
 # create dictionary for yaml
 def build_yaml(args, github_token):
     services = get_services()
     yaml = {}
+    devops = {}
+    migrations = {}
+    router = {}
+    inbound = {}
+
     for repo in get_repos(github_token):
+        if not get_branch(repo, args.release):
+            continue
+
         service = repo.name.replace('telematics-hub-','')
         if re.match(r'dev-ops|common', service):
             continue
+
         sep = '_' if re.match(r'devops|migrations', service) else '-'
         service = services[service] if service in services.keys() else service
         job = "telhub%s%s" % (sep, get_job(service))
         dict = {job: {'commit': repo.get_branch('master').commit.sha, 'branch': args.release}}
+
         if service == 'devops':
             devops = dict
             devops[job].update({'run_config': True, 'wait': 1})
         elif service == 'migrations':
             migrations = dict
+        elif service == 'messages-router':
+            router = dict
+        elif service == 'inbound-messages-api':
+            inbound = dict
         else:
             yaml.update(dict)
+
     yaml[job].update({'wait': 1})
-    return {'jobs': {**devops, **migrations, **yaml}}
+    return {'jobs': {**devops, **migrations, **router, **inbound, **yaml}}
 
 # build the yaml for jenkins to deploy from
 def create_yaml(args, github_token):
